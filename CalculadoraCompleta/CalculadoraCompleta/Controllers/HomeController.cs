af545c0f25bca5c0d33781bc2a7dcf55e3186b93
andreiaferreiraa <aluno19985@ipt.pt>
2018-03-09 17:06:41 +0000

    criação de interface gráfica

---
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraCompleta.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet] //facultativo, porque por defeito é sempre
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //POST:Home
+        [HttpPost]
+        public ActionResult Index(string bt, string display )
+        {
+            return View();
+        }
+    }
+}
